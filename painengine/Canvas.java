ba34d531a51ec8f00a35f8d6913ac0596b68ae85
HeyyD <heidi.hati@cs.tamk.fi>
2017-11-03 16:09:53 +0200

    Base for Canvas

--- painengine/Canvas.java
+++ painengine/Canvas.java
@@ -4,30 +4,37 @@ import java.util.List;
 
 
 /**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
+	Canvas contains all the Sprites and gameobjects that are
+	drawn to the Screen. It can extend outside of the screen.
+	Canvas can contain multiple CanvasLayers that can be
+	drawn on top of eachother.
  */
 
 public class Canvas
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+
+	private int width;
+	private int height;
+
 	private List<CanvasLayer> layers;
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public Canvas(){
-		super();
+	public Canvas(int width, int height){
+		setWidth(width);
+		setHeight(height);
 	}
 
+	public void setWidth(int width){
+		if(width > 0)
+			this.width = width;
+	}
+
+	public int getWidth() {return this.width;}
+
+	public void setHeight(int height){
+		if(height > 0)
+			this.height = height;
+	}
+
+	public int getHeight() {return this.height;}
 }
 
