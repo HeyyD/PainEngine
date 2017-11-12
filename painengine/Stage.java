193465012237caa99f7ee36943a0dbc5ce6a75f0
HeyyD <heidi.hati@cs.tamk.fi>
2017-11-12 19:06:06 +0200

    New class Stage
    -Add stage class

---
+++ painengine/Stage.java
@@ -0,0 +1,10 @@
+package painengine;
+
+/**
+    Stage contains canvas and all the needed information
+    for the gameObjects. Stages can be used for example
+    when switching levels.
+ */
+public class Stage{
+
+}
