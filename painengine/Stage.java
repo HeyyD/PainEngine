91c36fea76b5a8cabac1a232a82e93b628a66f61
HeyyD <heidi.hati@cs.tamk.fi>
2017-11-13 18:11:10 +0200

    Add new class to demo

--- painengine/Stage.java
+++ painengine/Stage.java
@@ -1,15 +1,26 @@
 package painengine;
 
+import painengine.gameobject.GameObject;
+
+import java.util.List;
+import java.util.LinkedList;
 /**
     Stage contains canvas and all the needed information
     for the gameObjects. Stages can be used for example
     when switching levels.
- */
+*/
+
 public class Stage{
     private Canvas canvas;
+    private List<GameObject> gameObjects = new LinkedList<>();
 
     public Stage(Canvas canvas){
         this.canvas = canvas;
+    }
+
+    public void addGameObject(GameObject o){
+        gameObjects.add(o);
+        getCanvas().addSprite(o);
     }
 
     public void setCanvas(Canvas canvas) {this.canvas = canvas;}
