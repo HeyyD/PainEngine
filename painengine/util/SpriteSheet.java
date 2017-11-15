b549d742be710080567a065aab9341c21085f7ef
HeyyD <heidi.hati@cs.tamk.fi>
2017-11-15 13:53:31 +0200

    Constructor for SpriteSheet

--- painengine/util/SpriteSheet.java
+++ painengine/util/SpriteSheet.java
@@ -1,5 +1,9 @@
 package painengine.util;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 /**
 	SpriteSheet class divides a image into columns and rows
@@ -10,8 +14,26 @@ package painengine.util;
 public class SpriteSheet
 {
 
-	public SpriteSheet(){
-		super();
+	private BufferedImage spriteSheet;
+	private BufferedImage[][] sprites;
+	private int rows;
+	private int columns;
+
+	public SpriteSheet(String path, int rows, int columns){
+		try{
+			spriteSheet = ImageIO.read(new File(path));
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+
+		this.rows = rows;
+		this.columns = columns;
+
+		sprites = splitSheet();
+	}
+
+	private BufferedImage[][] splitSheet(){
+		return null;
 	}
 
 }
