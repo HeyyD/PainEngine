e278d42df439286962a751735718f3b56dacd8d8
HeyyD <heidi.hati@cs.tamk.fi>
2017-11-21 20:41:56 +0200

    Screen class documentation

--- painengine/Screen.java
+++ painengine/Screen.java
@@ -3,20 +3,31 @@ import javax.swing.JFrame;
 import java.awt.Color;
 
 /**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
+	Screen class is the window that the game is drawn on.
+	@see JFrame
  */
 
 public class Screen extends JFrame
 {	
+	/** Current stage of the game */
 	private Stage stage;
 
+	/**
+		Constructor of the screen. It sets the name and 
+		size of the screen. It also sets it on the center
+		of the monitor and creates default stage and canvas
+		so that the user can start building the game.
+
+		@param name Name of the game screen
+		@param width Width of the screen
+		@param height Height of the screen
+	 */
 	public Screen(String name, int width, int height){
 		setTitle(name);
 		setSize(width, height);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
 		setFocusable(true);
 		Canvas canvas = new Canvas(width, height);
 		canvas.setBackground(Color.BLACK);
@@ -26,8 +37,14 @@ public class Screen extends JFrame
 		setVisible(true);
 	}
 
+	/**
+		@return Current stage of the game
+	 */
 	public Stage getStage(){return this.stage;}
 
+	/**
+		Set current stage of the game
+	 */
 	public void setStage(Stage stage){
 		if(this.stage != null)
 			remove(this.stage.getCanvas());
