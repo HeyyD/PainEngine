de82f01402c8a80ef3369804af0a75246ac844a2
HeyyD <heidi.hati@cs.tamk.fi>
2017-11-24 17:08:19 +0200

    Base for Rigidbody and file name change

--- demo/Player.java
+++ demo/Player.java
@@ -5,6 +5,7 @@ import painengine.gameobject.GameObject;
 import painengine.component.Collider;
 import painengine.component.Animation;
 import painengine.component.Animator;
+import painengine.component.Rigidbody;
 
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
@@ -22,44 +23,27 @@ public class Player extends GameObject{
     private Collider collider = new Collider();
     private SpriteSheet spriteSheet = new SpriteSheet("demo/assets/spritesheet.png", 4, 8);
     private Animator anim = new Animator(spriteSheet);
+    private Rigidbody rb = new Rigidbody();
 
     public Player(BufferedImage image, int x, int y){
         super(image, x, y);
 
         addComponent(collider);
         addComponent(anim);
+        addComponent(rb);
 
         anim.addAnimation("left", 0, 8);
-        anim.addAnimation("right", 9, 16);
-        anim.addAnimation("up", 16, 24);
-        anim.addAnimation("down", 24, 32);
         
-        anim.play("down");
+        anim.play("left");
     }
 
     @Override
     public void update(){
         move();
-
-        if(collider.collides(enemyColliders))
-            System.out.println("ouch");
-        if(collider.collides(walls))
-            System.out.println("wall");
     }
 
     private void move(){
-        if(isKeyPressed(KeyEvent.VK_A)){
-            setX(getX() - speed); anim.play("left");
-        }
-        else if (isKeyPressed(KeyEvent.VK_D)){
-             setX(getX() + speed); anim.play("right");
-        }
-        else if (isKeyPressed(KeyEvent.VK_W)){
-             setY(getY() - speed); anim.play("up");
-        }
-        else if (isKeyPressed(KeyEvent.VK_S)){
-             setY(getY() + speed); anim.play("down");
-        }
+
     }
 
     public void setWalls(List<Rectangle> walls) {this.walls = walls;}
---
+++ painengine/component/Rigidbody.java
@@ -0,0 +1,16 @@
+package painengine.component;
+
+public class Rigidbody extends GameComponent
+{
+
+	public Rigidbody(){
+		super();
+	}
+
+	@Override
+	public void run(){
+		System.out.println("here");
+	}
+
+}
+
